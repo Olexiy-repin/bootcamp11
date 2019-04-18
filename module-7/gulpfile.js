ff37707b1a078c93526b5a2573583dfc004a5d43
Olexiy <olexiy@Olexiys-MacBook-Pro.local>
2019-04-18 10:50:39 +0300

    add module-7

--- module-7/gulpfile.js
+++ module-7/gulpfile.js
@@ -30,10 +30,48 @@ gulp.task('js', function(){
     }));
 });
 
+// TASK FOR CSS
+gulp.task('css', function(){
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(mmq({
+      log: true
+    }))
+    .pipe(gulp.dest('./dist'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
+// TASK FOR IMAGES
+gulp.task('img', function(){
+  return gulp.src('./src/img/**/*.+(png|gif|svg|jpg)')
+    .pipe(imagemin(
+      imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+      })
+    ))
+    .pipe(gulp.dest('./dist/img'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
 // TASK FOR WATCH
 gulp.task('watch', function(){
   gulp.watch('./src/html/**/*.html', ['html']);
   gulp.watch('./src/js/**/*.js', ['js']);
+  gulp.watch('./src/sass/**/*.scss', ['css']);
+  gulp.watch('./src/img/**/*.*', ['img']);
 });
 
 // TASK FOR LIVE SERVER
@@ -52,7 +90,7 @@ gulp.task('del:dist', function(){
 });
 
 // BUILD TASK
-gulp.task('build', ['html', 'js']);
+gulp.task('build', ['html', 'js', 'css', 'img']);
 
 // START TASK
 gulp.task('start', ['del:dist', 'build', 'server', 'watch']);
--- module-7/src/js/main.js
+++ module-7/src/js/main.js
@@ -1,488 +1,65 @@
-// 'use strict'
-// //
-// // const he = {
-// //   name: 'Bond',
-// //   number: 7,
-// //   getName() {
-// //     console.log(this.number);
-// //   },
-// // }
-// //
-// // const she = Object.create(he);
-// //
-// // she.name = 'Julia';
-// // she.age = 50;
-// // she.boobsPower = 4;
-// //
-// // console.log(he);
-// // console.log(she);
-// //
-// // for (let key in she) {
-// //   if (she.hasOwnProperty(key)) {
-// //     console.log(key);
-// //   }
-// // }
-// //
-// // he.getName();
-//
-// function DRAGON(name, level) {
-//   this.name = name;
-//   this.level = level;
-// }
-//
-// DRAGON.prototype.getLevel = function(el) {
-//   return this.level += el;
-// }
-//
-// const dragon = new DRAGON('Bond', 80);
-// dragon.getLevel(10);
-//
-// console.log(dragon);
-//
-// function DubleDragon(name, level,weapon) {
-//
-//   DRAGON.call(this, name, level);
-//   this.weapon = weapon;
-//
-// }
-//
-// const dubleDragon = new DubleDragon('Maks', 9999, 'falos');
-//
-// DubleDragon.prototype.newName = function(value) {
-//   return this.name = value;
-// }
-//
-// dubleDragon.newName('Ben');
-//
-// console.log(dubleDragon);
-//
-//
-
-
-// class MyClass {
-//     constructor(name, xp, level) {
-//       this.name = name;
-//       this.xp = xp;
-//       this.level = level;
-//     }
-//
-//     getName() {
-//       console.log(this.name);
-//     }
-//
-//     minus(el) {
-//       return this.xp -= el;
-//     }
-// }
-//
-// class NewClass extends MyClass {
-//   constructor(name, xp, level, weapon) {
-//     super(name, xp, level);
-//     this.weapon = weapon;
-//   }
-// }
-//
-// const myClass = new MyClass('Bond', 100, 80);
-//
-// const newClass = new NewClass('Maksim', 50 , 20, 'Iron');
-//
-// console.log(newClass);
-// myClass.minus(10);
-// console.log(myClass);
-
-// const doubleClass = new MyClass('Maksim', 20, 60);
-//
-// doubleClass.minus(20);
-//
-// console.log(doubleClass);
-
-// =================== TASKS ===================
-
-// TASK 01
-/*
-  Напиши функцию-конструктор Account, которая добавляет будущему
-  объекту поля login, email.
-
-  В prototype функции-конструктора добавь метод getInfo(),
-  который выводит в консоль значения полей login и email.
-
-  Обрати внимание, метод всего один, в поле prototype функции-конструктора,
-  а использовать его смогут все экземпляры, по ссылке.
-
-  Создать несколько экземпляров с разными значениями свойств, вывесди их в консоль.
-*/
-
-// function Account(login, email) {
-//   this.login = login;
-//   this.email = email;
-// }
-//
-// Account.prototype.getInfo = function() {
-//   console.log(`${this.login} : ${this.email}`);
-// }
-//
-// const account = new Account('Mangozedog', 'mango@dog.woof');
-//
-// console.log(Account.prototype.getInfo); // function
-// account.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
-// TASK 02
-/*
-  Напиши ES6 класс StringBuilder.
-
-  На вход (в конструкторе) он получает один параметр string - строку,
-  которую записывает в свойство _value.
-
-  Добавь классу следующие методы:
-
-    - геттер value - возвращает текущее значение поля _value
-
-    - append(str) - получает парметр str (строку) и добавляет ее в конец _value
-
-    - prepend(str) - получает парметр str (строку) и добавляет ее в начало value
-
-    - pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
-*/
-
-// class StringBuilder {
-//   constructor(str) {
-//     this._value = str;
-//   }
-//
-//   get value() {
-//     return this._value;
-//   }
-//
-//   append(str) {
-//     return this._value += str;
-//   }
-//
-//   prepend(str) {
-//     return this._value = str += this._value;
-//   }
-//
-//   pad(str) {
-//     return this._value = str + this._value + str;
-//   }
-// }
-//
-// const builder = new StringBuilder('.');
-//
-// console.log(builder.value);
-// builder.append('^');
-// console.log(builder.value); // '.^'
-// //
-// builder.prepend('^');
-// console.log(builder.value); // '^.^'
-// //
-// builder.pad('=');
-// console.log(builder.value); // '=^.^='
-
-// TASK 03
-/*
- * Напиши класс Car с указанными свойствами и методами
-*/
-
-// class Car {
-//   constructor({ maxSpeed = 0 }) {
-//     /*
-//       Добавь свойства:
-//         - speed - для отслеживания текущей скорости, изначально 0.
-//         - maxSpeed - для хранения максимальной скорости
-//         - running - для отслеживания заведен ли автомобиль, возможные значения true или false. Изначально false.
-//         - distance - содержит общий киллометраж, изначально с 0
-//     */
-//     this.speed = 0;
-//     this.maxSpeed = maxSpeed;
-//     this._running = false;
-//     this.distance = 0;
-//   }
-//
-//   get running() {
-//     return this._running;
-//   }
-//
-//   turnOn() {
-//     // Добавь код для того чтобы завести автомобиль
-//     // Просто записывает в свойство running значание true
-//     this._running = true;
-//     return true;
-//   }
-//
-//   turnOff() {
-//     // Добавь код для того чтобы заглушить автомобиль
-//     // Просто записывает в свойство running значание false
-//     this._running = false;
-//     return false;
-//   }
-//   //
-//   accelerate(spd) {
-//     // Записывает в поле speed полученное значение, при условии что
-//     // оно не больше чем значение свойства maxSpeed
-//     return this.speed = spd < this.maxSpeed ? spd : 0;
-//   }
-//   //
-//   decelerate(spd) {
-//     // Записывает в поле speed полученное значение, при условии что
-//     // оно не больше чем значение свойства maxSpeed и не меньше нуля
-//     return this.speed = spd < this.maxSpeed && spd > 0 ? spd : 0;
-//   }
-//   //
-//   drive(hours) {
-//     // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed),
-//     // но только в том случае если машина заведена!
-//     return this.distance = this._running ? hours * this.speed : 0;
-//   }
-// }
-//
-// const car = new Car({ maxSpeed: 100 });
-//
-// console.log(car.turnOn());
-//
-// console.log(car.turnOff());
-//
-// console.log(car.accelerate(10));
-//
-// console.log(car.decelerate(10));
-//
-// console.log(car.drive(20));
-
-// TASK 04
-/*
-* Добавь к классу Car из предыдущего задания статический
-* метод getSpecs, который принимает объект-машину как параметр
-* и выводит в консоль значения полей maxSpeed, speed, running и distance.
-*/
-//
-// class Car {
-//   constructor({ maxSpeed = 0 }) {
-//     this.speed = 0;
-//     this.maxSpeed = maxSpeed;
-//     this._running = false;
-//     this.distance = 0;
-//   }
-//
-//   get running() {
-//     return this._running;
-//   }
-//
-//   turnOn() {
-//     this._running = true;
-//     return true;
-//   }
-//
-//   turnOff() {
-//     this._running = false;
-//     return false;
-//   }
-//
-//   accelerate(spd) {
-//     return this.speed = spd < this.maxSpeed ? spd : 0;
-//   }
-//
-//   decelerate(spd) {
-//     return this.speed = spd < this.maxSpeed && spd > 0 ? spd : 0;
-//   }
-//
-//   drive(hours) {
-//     return this.distance = this._running ? hours * this.speed : 0;
-//   }
-//
-//   static getSpecs(obj) {
-//     console.log(`maxSpeed: ${obj.maxSpeed}, speed: ${obj.speed}, running: ${obj._running}, distance: ${obj.distance}`);
-//   }
-// }
-//
-// const car = new Car({ maxSpeed: 100 });
-// car.turnOn();
-// car.accelerate(50);
-// car.drive(2);
-//
-// Car.getSpecs(car); // maxSpeed: 100, speed: 50, running: true, distance: 100
-
-// TASK 05
-/*
-* Добавь классу Car свойство цены автомобиля, назови его сам.
-* Добавь геттер и сеттер value, который будет работать с свойством цены автомобиля.
-*/
-
-// class Car {
-//   constructor({ maxSpeed = 0, price = 0}) {
-//     this.speed = 0;
-//     this.maxSpeed = maxSpeed;
-//     this._running = false;
-//     this.distance = 0;
-//     this._price = price;
-//   }
-//
-//   get running() {
-//     return this._running;
-//   }
-//
-//   get price() {
-//     return this._price;
-//   }
-//
-//   set price(value) {
-//     return this._price = value;
-//   }
-//
-//   turnOn() {
-//     this._running = true;
-//     return true;
-//   }
-//
-//   turnOff() {
-//     this._running = false;
-//     return false;
-//   }
-//
-//   accelerate(spd) {
-//     return this.speed = spd < this.maxSpeed ? spd : 0;
-//   }
-//
-//   decelerate(spd) {
-//     return this.speed = spd < this.maxSpeed && spd > 0 ? spd : 0;
-//   }
-//
-//   drive(hours) {
-//     return this.distance = this._running ? hours * this.speed : 0;
-//   }
-//
-//   static getSpecs(obj) {
-//     console.log(`maxSpeed: ${obj.maxSpeed}, speed: ${obj.speed}, running: ${obj._running}, distance: ${obj.distance}`);
-//   }
-// }
-//
-// const car = new Car({ maxSpeed: 50, price: 2000 });
-// console.log(car.price); // 2000
-// //
-// car.price = 4000;
-// console.log(car.price); // 4000
-
+'use strict'
 // HOMEWORK
-const findPriceAndCalories = (obj, value, whatFind) => {
-  let realPrice = 0;
-  let realCalories = 0;
-
-  if (whatFind === 'price') {
-    for (let key in obj) {
-      if (key === value || value.includes(key)) {
-        realPrice += obj[key].price;
-      }
-    }
-    return realPrice;
-  } else if (whatFind === 'calories') {
-    for (let key in obj) {
-      if (key === value || value.includes(key)) {
-        realCalories += obj[key].calories;
-      }
-    }
-    return realCalories;
+const posts = [
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-1.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/nature",
+    title: "Post title 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-2.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-3.com'
   }
+];
+
+const createPostCard = (post) => {
+  let container = document.querySelector('.card-container');
+  let div = document.createElement('div');
+  let divBody = document.createElement('div');
+  let img = document.createElement('img');
+  let h2 = document.createElement('h2');
+  let pDesc = document.createElement('p');
+  let pDate = document.createElement('p');
+  let pRat = document.createElement('p');
+  let link = document.createElement('a');
+
+  div.className = 'movie';
+  container.append(div);
+  img.setAttribute('src', `${post.img}`);
+  img.setAttribute('alt', 'movie image');
+  img.className = 'movie__image';
+  div.prepend(img);
+  divBody.className = 'movie__body';
+  img.after(divBody);
+  h2.className = 'movie__title';
+  h2.innerHTML = `${post.title}`;
+  divBody.prepend(h2);
+  pDesc.className = 'movie__description';
+  pDesc.innerHTML = `${post.text}`;
+  h2.after(pDesc);
+  pDate.className = 'movie__date';
+  pDate.innerHTML = 'Released: 1972-03-14';
+  pDesc.after(pDate);
+  pRat.className = 'movie__rating';
+  pRat.innerHTML = 'Rating: 8.6';
+  pDate.after(pRat);
+  link.className = 'card-link';
+  link.innerHTML = `${post.link}`;
+  link.setAttribute('href', '#');
+  pRat.after(link);
 }
 
-class Hamburger {
-  constructor(size, stuffing) {
-    this._size = size;
-    this._stuffing = stuffing;
-    this._toppings = [];
-    this._calculatePrice = 0;
-    this._calculateCalories = 0;
-  }
-
-  addTopping(...args) {
-    this._toppings = [...new Set(this._toppings.concat(args))];
-    return this._toppings;
-  }
-
-  removeTopping(topping) {
-    this._toppings.includes(topping) ? this._toppings.splice(this._toppings.indexOf(topping), 1) : this._toppings;
-    return this._toppings;
-  }
-
-  get toppings() {
-    return this._toppings;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  get stuffing() {
-    return this._stuffing;
-  }
-
-  get calculatePrice() {
-    const gamSize = findPriceAndCalories(Hamburger.SIZES, this._size, 'price');
-    const gamStuffing = findPriceAndCalories(Hamburger.STUFFINGS, this._stuffing, 'price');
-    const gamToppings = findPriceAndCalories(Hamburger.TOPPINGS, this._toppings, 'price');
-    return gamSize + gamStuffing + gamToppings;
-  }
-
-  get calculateCalories() {
-    const gamSize = findPriceAndCalories(Hamburger.SIZES, this._size, 'calories');
-    const gamStuffing = findPriceAndCalories(Hamburger.STUFFINGS, this._stuffing, 'calories');
-    const gamToppings = findPriceAndCalories(Hamburger.TOPPINGS, this._toppings, 'calories');
-
-    return gamSize + gamStuffing + gamToppings;
-  }
-
+const createCards = posts => {
+  posts.forEach(el => createPostCard(el));
 }
 
-Hamburger.SIZE_SMALL = 'SIZE_SMALL';
-Hamburger.SIZE_LARGE = 'SIZE_LARGE';
-
-Hamburger.SIZES = {
-  [Hamburger.SIZE_SMALL]: {
-    price: 30,
-    calories: 50,
-  },
-  [Hamburger.SIZE_LARGE]: {
-    price: 50,
-    calories: 100,
-  },
-};
-
-Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
-Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
-Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
-
-Hamburger.STUFFINGS = {
-  [Hamburger.STUFFING_CHEESE]: {
-    price: 15,
-    calories: 20,
-  },
-  [Hamburger.STUFFING_SALAD]: {
-    price: 20,
-    calories: 5,
-  },
-  [Hamburger.STUFFING_MEAT]: {
-    price: 35,
-    calories: 15,
-  },
-};
-
-Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
-Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
-
-Hamburger.TOPPINGS = {
-  [Hamburger.TOPPING_SPICE]: {
-    price: 10,
-    calories: 0,
-  },
-  [Hamburger.TOPPING_SAUCE]: {
-    price: 15,
-    calories: 5,
-  },
-};
-
-const hamburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
-
-hamburger.addTopping(Hamburger.TOPPING_SPICE, Hamburger.TOPPING_SAUCE);
-
-// hamburger.removeTopping(Hamburger.TOPPING_SAUCE);
-
-console.log(hamburger.calculatePrice);
-console.log(hamburger.calculateCalories);
-console.log(hamburger);
+createCards(posts);
