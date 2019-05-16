ca51f42f9a2f45e73c8dbf855c58fd9c0609af9d
Olexiy <olexiy@Olexiys-MacBook-Pro.local>
2019-05-16 17:18:06 +0300

    update react hw 02

---
+++ react/homework/01/build/precache-manifest.39989b8a23a50e641000ed707c738556.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a9e27f8481ddbe89c38facfd4a910e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15c42e30ff394fdfef31",
+    "url": "./static/css/main.48b4e88a.chunk.css"
+  },
+  {
+    "revision": "1aa4d8a9f7e08f0e9dfe",
+    "url": "./static/js/2.31b2e26f.chunk.js"
+  },
+  {
+    "revision": "15c42e30ff394fdfef31",
+    "url": "./static/js/main.91784a04.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
